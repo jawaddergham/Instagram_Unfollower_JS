d50bc6fc226c9d92b12ce23c45be71f731f2f0e3
Jawad <jawaddergham13@outlook.com>
2021-11-04 20:41:49 -0400

    feat: Error handling
    Added error handling incase Instagram rate limit occurs, or anything else really, halts the program.

--- Instagram_Unfollow.js
+++ Instagram_Unfollow.js
@@ -1,5 +1,5 @@
 /*
-    Instagram Wannabe Celebs Unfollower
+    Instagram Unfollower
     Copyright (C) 2021 Jawad Dergham
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -30,40 +30,36 @@ function minutesToMilliseconds(minutes) {
 }
 
 async function webReq(url, post) {
-    let data;
+    let response = await fetch(url, {
+        "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "sec-ch-ua": "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "x-asbd-id": "198387",
+        "x-csrftoken": getCookie("csrftoken"),
+        "x-ig-app-id": "936619743392459",
+        "x-ig-www-claim": "hmac.AR3EpWdeQ8EKsT2EKxCGC6nCYkDo-dqyQU4Pqyp9b0w5Hazb",
+        "x-instagram-ajax": "74408599c4ff",
+        "x-requested-with": "XMLHttpRequest"
+        },
+        "referrer": window.location.href,
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": post ? "POST" : "GET",
+        "mode": "cors",
+        "credentials": "include"
+    });
 
-    try {
-        const response = await fetch(url, {
-            "headers": {
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "sec-ch-ua": "\"Google Chrome\";v=\"95\", \"Chromium\";v=\"95\", \";Not A Brand\";v=\"99\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            "x-asbd-id": "198387",
-            "x-csrftoken": getCookie("csrftoken"),
-            "x-ig-app-id": "936619743392459",
-            "x-ig-www-claim": "hmac.AR3EpWdeQ8EKsT2EKxCGC6nCYkDo-dqyQU4Pqyp9b0w5Hazb",
-            "x-instagram-ajax": "74408599c4ff",
-            "x-requested-with": "XMLHttpRequest"
-            },
-            "referrer": window.location.href,
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": post ? "POST" : "GET",
-            "mode": "cors",
-            "credentials": "include"
-        });
-    
-        data = await response.json()
-    } catch(err) {
-        data = "ERROR";
-    }
-
-    return data;
+    if (response.status === 200) {
+        return await response.json();
+    } else if (response.status == 429)
+        return "RATELIMIT";
+    else return "ERROR";
 }
 
 async function fillUsers(which) {
@@ -72,8 +68,14 @@ async function fillUsers(which) {
     
     while(true) {
         data = await webReq(url, false);
-    
         if (data != null) {
+            if (data == "ERROR" || data == "RATELIMIT") {
+                if (data == "RATELIMIT")
+                    throw new Error("You've exceeded the rate limit. Try again in 24 hours.");
+                else 
+                    throw new Error("Error detected. Haulting script.");
+            }
+
             if (data["page_size"] != null) {
                 var max_id = data["next_max_id"];
                 url = "https://i.instagram.com/api/v1/friendships/"+getCookie("ds_user_id")+"/"+which+"/?count=12&max_id="+max_id+"&search_surface=follow_list_page";
@@ -97,6 +99,15 @@ async function isCeleb(pk) {
     data = await webReq("https://i.instagram.com/api/v1/users/"+pk+"/info/", false);
 
     if (data != null) {
+        if (data == "ERROR" || data == "RATELIMIT") {
+            if (data == "RATELIMIT") {
+                console.log("You've exceeded the rate limit. We'll try again in 5 minutes.")
+
+                await new Promise(r => setTimeout(r, minutesToMilliseconds(5)));
+            } else
+                throw new Error("Error detected. Haulting script.");
+        }
+
         return (data["user"]["follower_count"] > FollowerCount);
     }
 }
@@ -119,47 +130,56 @@ function create_unfollowers_list(following, followers) {
 }
 
 var unfollow_req_ctr = 0;
-async function unfollow_celeb_wannabes(unfollow_list) {
-    for (var i = 0; i < unfollow_list.length; i++) {
-        if (unfollow_list[i][2])
-            continue;
-
-        var celeb = await isCeleb(unfollow_list[i][1]);
-
-        if (!celeb) {
-            data = await webReq("https://www.instagram.com/web/friendships/"+unfollow_list[i][1]+"/unfollow/", true);
-            
-            if (data == "ERROR") {
-                console.log("You've most likely hit a rate limit. We'll try again in 20 minutes.")
-                i -= 1;
-
-                await new Promise(r => setTimeout(r, minutesToMilliseconds(20)));
-
+async function unfollow(unfollow_list) {
+    try {
+        for (var i = 0; i < unfollow_list.length; i++) {
+            if (unfollow_list[i][2])
                 continue;
-            }
-            
-            console.log("Unfollowed "+unfollow_list[i][0]+i+"/"+unfollow_list.length+" left");
-            unfollow_req_ctr += 1;
-        }
 
-        if (unfollow_req_ctr == 15) {
-            unfollow_req_ctr = 0;
-            await new Promise(r => setTimeout(r, minutesToMilliseconds(12)));
+            if (!celeb) {
+                data = await webReq("https://www.instagram.com/web/friendships/"+unfollow_list[i][1]+"/unfollow/", true);
+                
+                if (data == "ERROR" || data == "RATELIMIT") {
+                    if (data == "RATELIMIT") {
+                        console.log("You've exceeded the rate limit. We'll try again in 20 minutes.")
+                        i -= 1;
+
+                        await new Promise(r => setTimeout(r, minutesToMilliseconds(20)));
+
+                        continue;
+                    }
+                    else throw new Error("Error detected. Haulting script.");
+                }
+                
+                console.log("Unfollowed "+unfollow_list[i][0]+". "+i+"/"+unfollow_list.length+" left");
+                unfollow_req_ctr += 1;
+            }
+
+            if (unfollow_req_ctr == 15) {
+                unfollow_req_ctr = 0;
+                await new Promise(r => setTimeout(r, minutesToMilliseconds(12)));
+            }
         }
+        console.log("Ran through the whole list. There's no more wannabe-celebs!");
+    } catch (err) {
+        throw new Error(err.message);
     }
-    console.log("Ran through the whole list. There's no more wannabe-celebs!");
 }
 
 async function main() {
-    var followers_arr = await fillUsers("followers");
+    try {
+        var followers_arr = await fillUsers("followers");
 
-    await new Promise(r => setTimeout(r, minutesToMilliseconds(4))); 
+        await new Promise(r => setTimeout(r, minutesToMilliseconds(4))); 
 
-    var following_arr = await fillUsers("following");
+        var following_arr = await fillUsers("following");
 
-    var not_following_me_arr = create_unfollowers_list(following_arr, followers_arr);
+        var not_following_me_arr = create_unfollowers_list(following_arr, followers_arr);
 
-    await unfollow_celeb_wannabes(not_following_me_arr);
+        await unfollow(not_following_me_arr);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 await main();
